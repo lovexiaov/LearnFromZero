052719be9d0f238b7f1341e07c0938fd5113d6c1
lovexiaov <cantywei@qq.com>
2015-12-02 22:28:38 +0800

    add README.md，命名规则.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Learn Android From Zero
+
+是系统的从头学习时候了，本次学习有以下几个目的：
+
+- 学习 Android 基础知识
+- 学习 Android Testing 技巧
+- 学习 Gradle 基础知识
+- 学习 Git 基础知识
+- 学习 SVN 基础知识
+
+- 学习 Android 自定义 View
+- 其他
+
+学习过程中会有笔记文档输出，存放在 `doc` 文件夹中。
--- app/src/main/java/com/lovexiaov/learnfromzero/AtyMain.java
+++ app/src/main/java/com/lovexiaov/learnfromzero/AtyMain.java
@@ -3,11 +3,11 @@ package com.lovexiaov.learnfromzero;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Aty_amin extends AppCompatActivity {
+public class AtyMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.aty_amin);
+        setContentView(R.layout.aty_main);
     }
 }
