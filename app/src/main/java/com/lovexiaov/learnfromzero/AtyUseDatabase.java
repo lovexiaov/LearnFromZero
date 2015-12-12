102ec45828bf315d075b4fee519335c697cddc7f
lovexiaov <cantywei@qq.com>
2015-12-12 16:02:56 +0800

    Use SqliteOpenHelper replace DatabaseUtils to interact with SqliteDataBase

--- app/src/main/java/com/lovexiaov/learnfromzero/AtyUseDatabase.java
+++ app/src/main/java/com/lovexiaov/learnfromzero/AtyUseDatabase.java
@@ -1,9 +1,15 @@
 package com.lovexiaov.learnfromzero;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.lovexiaov.learnfromzero.db.MyDatabaseHelper;
 
 /**
  * Aty to use database
@@ -11,18 +17,14 @@ import android.view.View;
  */
 public class AtyUseDatabase extends AtyBase {
 
-    public static final String CREATE_BOOK = "create table Book ("
-            + "id integer primary key autoincrement,"
-            + "author text,"
-            + "price real,"
-            + "page integer,"
-            + "name text)";
+    private MyDatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_use_database);
 //        DatabaseUtils.createDbFromSqlStatements(AtyUseDatabase.this,"test.db", 1, CREATE_BOOK);
+        dbHelper = new MyDatabaseHelper(this, "test.db", null, 1);
     }
 
     public static void actionStart(Context context) {
@@ -31,18 +33,42 @@ public class AtyUseDatabase extends AtyBase {
     }
 
     public void add(View view) {
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
 
+        ContentValues values = new ContentValues();
+        values.put("author", "lovexiaov");
+        values.put("price", 99.0);
+        values.put("page", 399);
+        values.put("name", "Learn Android From 0");
+        database.insert(MyDatabaseHelper.TABLE_BOOK, null, values);
+        Toast.makeText(AtyUseDatabase.this, "add data", Toast.LENGTH_SHORT).show();
+        database.close();
     }
 
     public void update(View view) {
 
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        ContentValues  values   = new ContentValues();
+        values.put("price", 9.9);
+        database.update(MyDatabaseHelper.TABLE_BOOK, values, "name = ?", new String[]{"Learn Android From 0"});
+        database.close();
     }
 
     public void delete(View view) {
-
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        database.delete(MyDatabaseHelper.TABLE_BOOK, "name = ?", new String[]{"Learn Android From 0"});
+        database.close();
     }
 
     public void retrieve(View view) {
-
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        Cursor         cursor   = database.query(MyDatabaseHelper.TABLE_BOOK, null, null, null, null, null, null);
+        int            count    = 0;
+        while (cursor.moveToNext()) {
+            count++;
+        }
+        cursor.close();
+        database.close();
+        Toast.makeText(AtyUseDatabase.this, "total: " + count, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/lovexiaov/learnfromzero/db/MyDatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.lovexiaov.learnfromzero.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Database helper
+ * Created by lovexiaov on 15/12/10.
+ */
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context mContext;
+
+    public static final String TABLE_BOOK  = "BOOK";
+    public static final String BOOK_AUTHOR = "AUTHOR";
+    public static final String BOOK_PRICE  = "PRICE";
+    public static final String BOOK_PAGE   = "PAGE";
+    public static final String BOOK_NAME   = "NAME";
+
+    private final String CREATE_BOOK =
+            "create table " + TABLE_BOOK + " ("
+            + "id integer primary key autoincrement,"
+            + BOOK_AUTHOR + " text,"
+            + BOOK_PRICE + " real,"
+            + BOOK_PAGE + " integer,"
+            + BOOK_NAME + " text)";
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_BOOK);
+        Toast.makeText(mContext, "Create db success", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
